5408d7f3f7727b64a1bd4d62a31764bc33aa41b5
JC <>
2018-04-30 20:06:43 +0800

    first submit

---
+++ src/main/java/com/jc/project/App.java
@@ -0,0 +1,13 @@
+package com.jc.project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/jc/project/Test.java
@@ -0,0 +1,11 @@
+package com.jc.project;
+
+/**
+ * Created by JC on 2018/4/30.
+ */
+public class Test {
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
