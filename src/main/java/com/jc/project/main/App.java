5b88651b523eb83d07ef97fafbfcd620e311cf4a
JC <>
2018-04-30 19:13:51 +0800

    update .gitignore

---
+++ src/main/java/com/jc/project/main/App.java
@@ -0,0 +1,11 @@
+package com.jc.project.main;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+}
